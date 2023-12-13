cdfbd06d5fa54f373b182b8f960df660919cf1b4
Gabi Balasz <53608592+gbalasz-metro@users.noreply.github.com>
2023-12-13 23:06:44 +0200

    feat(setup): ✨ configured stylex

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,30 @@
+/** @type {import('next').NextConfig} */
+
 import bundleAnalyzer from '@next/bundle-analyzer';
+import stylexPlugin from '@stylexjs/nextjs-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const nextConfig = {
+    reactStrictMode: false,
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    experimental: {
+      optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
+    },
+    pageExtensions: ["js", "jsx", "ts", "tsx", "json"],
+  }
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
-  reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
-});
+export default stylexPlugin({
+  rootDir: __dirname,
+})(
+  withBundleAnalyzer(nextConfig)
+);
--- src/app/crm/page.tsx
+++ src/app/crm/page.tsx
@@ -1,13 +1,19 @@
 'use client';
 
-import { PublicHeader } from '@uiComponents/public/PublicHeader/PublicHeader';
 import { Welcome } from '@uiComponents/public/Welcome/Welcome';
+import stylex from '@stylexjs/stylex';
+import { colors } from '@stylexjs/open-props/lib/colors.stylex';
 
 export default function DashboardPage() {
+  const x = stylex.create({
+    test: {
+      color: colors.red5,
+    },
+  });
   return (
-    <>
-      {/* <PublicHeader /> */}
+    <main className={stylex( x.test )}>
       <Welcome />
-    </>
+      <div>example</div>
+    </main>
   );
 }
